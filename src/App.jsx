cb96dc7d1e48f32958b6f2d9a5f92363ebfa977a
sainul-abid-tk <sainulabidtk2468@gmail.com>
2024-01-19 10:04:48 +0530

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import io from "socket.io-client"
 import Chat from './Chat'
-const socket = io.connect("http://localhost:3000")
+const socket = io.connect("https://chat-server-iqjo.onrender.com")
 function App() {
   const [username,setUsername]=useState("")
   const [roomId,setRoomId]=useState("")
